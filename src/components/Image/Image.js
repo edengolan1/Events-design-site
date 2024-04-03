4ec33771c52fff4c113797233efaee4a0c7bcaeb
Eden Golan <eden9123@gmail.com>
2024-04-03 23:49:01 +0300

    change path images

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Image.css';
 
+const image1 = '/assets/image-wedding.jpg';
+const image2 = '/assets/image-wedding2.jpg';
+const image3 = '/assets/image-wedding3.jpg';
+
 function Image() {
     return (
         <div className='image-wedding'>
-            <div className='image1'></div>
-            <div className='image2'>WELCOME TO EVENTS DESIGN</div>
-            <div className='image3'></div>
+            <div className='image1' style={{backgroundImage: `url(${image1})`}}></div>
+            <div className='image2' style={{backgroundImage: `url(${image2})`}}>WELCOME TO EVENTS DESIGN</div>
+            <div className='image3' style={{backgroundImage: `url(${image3})`}}></div>
         </div>
     );
 }
